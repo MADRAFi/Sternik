e336a0fd32ccb0504966f1d283f5a0d4a412962d
MADRAFi <madrafi@gmail.com>
2022-07-17 21:57:45 +0200

    Initial Commit

---
+++ Shared/SternikApp.swift
@@ -0,0 +1,17 @@
+//
+//  SternikApp.swift
+//  Shared
+//
+//  Created by MADRAFi on 17/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SternikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
